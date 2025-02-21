b0950a58603c8b079099bae8a8fee5ee6d21e403
Himanshu Kumar <honey6k6@gmail.com>
2025-02-21 11:07:11 +0530

    Single Soldier Page done

---
+++ src/components/DynamicGraph.jsx
@@ -0,0 +1,78 @@
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../MainComponent";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const DynamicGraph = ({ selectedMetric }) => {
+  const { solData } = useContext(AuthContext);
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    if (solData !== undefined) {
+      setChartData((prevData) => {
+        const newValue =
+          selectedMetric == "Efficiency"
+            ? solData.efficiency_predictions[0]
+            : Math.floor(solData.soldier_data[selectedMetric]["0"]);
+        const newData = [...prevData, newValue];
+
+        if (newData.length > 10) newData.shift();
+        return newData;
+      });
+    }
+  }, [solData, selectedMetric]); // Re-run when `solData` or `selectedMetric` changes
+
+  const labels = chartData.map((_, index) => index + 1);
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: selectedMetric,
+        data: chartData,
+        fill: false,
+        backgroundColor: "#504b38",
+        borderColor: "#504b385c",
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: { position: "top" },
+      title: { display: true, text: `${selectedMetric} Over Time` },
+    },
+  };
+
+  return (
+    <div>
+      <Line
+        options={options}
+        data={data}
+        className="bg-[#F8F3D9] rounded-md p-6 "
+      />
+    </div>
+  );
+};
+
+export default DynamicGraph;
--- src/pages/SingleSol.jsx
+++ src/pages/SingleSol.jsx
@@ -1,10 +1,106 @@
+import { useState, useContext } from "react";
 import NavBar from "../components/NavBar";
+import CardUpper from "../components/CardUpper";
+import DynamicGraph from "../components/DynamicGraph";
+import { AuthContext } from "../MainComponent";
+
 const SingleSol = () => {
+  const { solData } = useContext(AuthContext);
+  const [selectedMetric, setSelectedMetric] = useState("Temperature"); // Default metric
+
+  const handleCardClick = (metric) => {
+    setSelectedMetric(metric);
+  };
+
   return (
     <div>
       <NavBar />
-      <h1>Single Soldier DashBoard Page</h1>
+      <h2 className="mainDH text-4xl text-center m-5">
+        Single Soldier Dashboard Page
+      </h2>
+      <div className="grid grid-cols-2 mb-5">
+        <div className="w-[30vw] grid grid-cols-2 gap-3 m-5 ml-20">
+          <div onClick={() => handleCardClick("efficiency_predictions")}>
+            <CardUpper
+              title="Efficiency"
+              value={solData.efficiency_predictions[0]}
+            />
+          </div>
+          <div onClick={() => handleCardClick("Temperature")}>
+            <CardUpper
+              title="Temperature"
+              value={Math.floor(solData.soldier_data.Temperature["0"])}
+            />
+          </div>
+          <div onClick={() => handleCardClick("Moisture")}>
+            <CardUpper
+              title="Moisture"
+              value={Math.floor(solData.soldier_data.Moisture["0"])}
+            />
+          </div>
+          <div onClick={() => handleCardClick("Water_Content")}>
+            <CardUpper
+              title="Water Content"
+              value={Math.floor(solData.soldier_data.Water_Content["0"])}
+            />
+          </div>
+          <div onClick={() => handleCardClick("SpO2")}>
+            <CardUpper
+              title="SpO2"
+              value={Math.floor(solData.soldier_data.SpO2["0"])}
+            />
+          </div>
+          <div onClick={() => handleCardClick("Fatigue")}>
+            <CardUpper
+              title="Fatigue"
+              value={Math.floor(solData.soldier_data.Fatigue["0"])}
+            />
+          </div>
+        </div>
+        <div className="absolute top-[180px] right-[70px] w-[52vw]">
+          <DynamicGraph selectedMetric={selectedMetric} />
+        </div>
+      </div>
+      <div className="grid grid-cols-6 p-5 ml-15 mr-8">
+        <div onClick={() => handleCardClick("Drowsiness")}>
+          <CardUpper
+            title="Drowsiness"
+            value={Math.floor(solData.soldier_data.Drowsiness["0"])}
+          />
+        </div>
+        <div onClick={() => handleCardClick("Heart_Rate")}>
+          <CardUpper
+            title="Heart Rate"
+            value={Math.floor(solData.soldier_data.Heart_Rate["0"])}
+          />
+        </div>
+        <div onClick={() => handleCardClick("Stress")}>
+          <CardUpper
+            title="Stress"
+            value={Math.floor(solData.soldier_data.Stress["0"])}
+          />
+        </div>
+        <div onClick={() => handleCardClick("Respiration_Rate")}>
+          <CardUpper
+            title="Respiration Rate"
+            value={Math.floor(solData.soldier_data.Respiration_Rate["0"])}
+          />
+        </div>
+        <div onClick={() => handleCardClick("Systolic_BP")}>
+          <CardUpper
+            title="Systolic BP"
+            value={Math.floor(solData.soldier_data.Systolic_BP["0"])}
+          />
+        </div>
+        <div onClick={() => handleCardClick("Diastolic_BP")}>
+          <CardUpper
+            title="Diastolic BP"
+            value={Math.floor(solData.soldier_data.Diastolic_BP["0"])}
+          />
+        </div>
+      </div>
     </div>
   );
 };
+
 export default SingleSol;
